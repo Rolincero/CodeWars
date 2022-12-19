0b6833df93e925ffa4cb3f40e3c72c0b335b1f92
Rolincero <ms.mayron29@gmail.com>
2022-12-19 21:41:37 +1000

    Добавьте файлы проекта.

---
+++ CodeWars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
