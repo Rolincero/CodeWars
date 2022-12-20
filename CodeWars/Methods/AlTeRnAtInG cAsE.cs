afdc91c5d82e8c274318b8d3144860c511da9340
Rolincero <ms.mayron29@gmail.com>
2022-12-20 20:31:06 +1000

    Alternating case ON DELAY

---
+++ CodeWars/Methods/AlTeRnAtInG cAsE.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Methods
+{
+    internal class AlTeRnAtInG_cAsE
+    {
+        public static string ToAlternatingCase(this string s)
+        {
+            //StringBuilder sb = new StringBuilder();
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sb.Append(s[i].ToString().ToUpper());
+            //    }
+            //    else
+            //    {
+            //        sb.Append(s[i].ToString());
+            //    }
+            //}
+            //return s;
+
+            // TODO
+        }
+    }
+}
