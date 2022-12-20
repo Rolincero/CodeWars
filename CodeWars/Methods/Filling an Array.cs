281dc9443b81c7ba985d67295fb48083188b3703
Rolincero <ms.mayron29@gmail.com>
2022-12-20 20:36:15 +1000

    Filling array COMPLETED

---
+++ CodeWars/Methods/Filling an Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Methods
+{
+    internal class Filling_an_Array
+    {
+       /* We want an array, but not just any old array, an array with contents!
+        *
+        *  Write a function that produces an array with the numbers 0 to N-1 in it.
+        *
+        *  For example, the following code will result in an array containing the numbers 0 to 4:
+        */
+
+        public static int[] Arr(int N)
+        {
+            int[] array = new int[N];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+            return array;
+        }
+    }
+}
--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -11,9 +11,7 @@ namespace CodeWars
     {
         static void Main()
         {
-            string str = Console.ReadLine();
-            string ending = Console.ReadLine();
-            Console.WriteLine(String_ends_with.Solution(str, ending));
+            
         }
     }
 }
