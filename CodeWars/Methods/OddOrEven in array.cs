0906d2c9df2ec497aa26b2db14c3f9a1837ff979
Rolincero <ms.mayron29@gmail.com>
2022-12-20 21:01:28 +1000

    Odd or even in array COMPLETED

---
+++ CodeWars/Methods/OddOrEven in array.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Methods
+{
+    internal class OddOrEven_in_array
+    {
+        public static string OddOrEven(int[] array)
+        {
+            if (array.Sum() % 2 == 0) return "even";
+            else return "odd";
+        }
+    }
+}
